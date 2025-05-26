f4c5fd5da511e00482cadf0d545bdc7d18aa62e9
David LJ <mail@davidlj95.com>
2025-05-27 01:27:06 +0200

    refactor: use tests instead of printing

--- src/main/java/com/davidlj95/MergeStringsAlternately.java
+++ src/main/java/com/davidlj95/MergeStringsAlternately.java
@@ -2,20 +2,6 @@ package com.davidlj95;
 
 // https://leetcode.com/problems/merge-strings-alternately (1768)
 public class MergeStringsAlternately {
-    public static void main(String[] args) {
-        System.out.println("Merge strings alternately:");
-        System.out.println("=========================");
-        String[][] TEST_CASES = {{"abc", "pqr"}, {"ab", "pqrs"}, {"abcd", "pq"}};
-        System.out.printf("%s | %s%n", "byLoopAndCheck", "commonThenAddRest");
-        for (var testCase : TEST_CASES) {
-            System.out.printf(
-                    "%s | %s\n",
-                    MergeStringsAlternately.byLoopAndCheck(testCase[0], testCase[1]),
-                    MergeStringsAlternately.commonAndThenRest(testCase[0], testCase[1])
-            );
-        }
-    }
-
     public static String byLoopAndCheck(String word1, String word2) {
         var result = new StringBuilder();
         var maxLength = Math.max(word1.length(), word2.length());
@@ -37,10 +23,10 @@ public class MergeStringsAlternately {
         for (var i = 0; i < commonLength; i++) {
             result.append(word1.charAt(i)).append(word2.charAt(i));
         }
-        if(word1.length() > commonLength) {
+        if (word1.length() > commonLength) {
             result.append(word1.substring(commonLength));
         }
-        if(word2.length() > commonLength) {
+        if (word2.length() > commonLength) {
             result.append(word2.substring(commonLength));
         }
         return result.toString();
---
+++ src/test/java/com/davidlj95/MergeStringsAlternatelyTest.java
@@ -0,0 +1,31 @@
+package com.davidlj95;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.FieldSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MergeStringsAlternatelyTest {
+    @SuppressWarnings({"unused"})
+    static final TestCase[] TEST_CASES = {
+            new TestCase("abc", "pqr", "apbqcr"),
+            new TestCase("ab", "pqrs", "apbqrs"),
+            new TestCase("abcd", "pq", "apbqcd"),
+    };
+
+    @ParameterizedTest
+    @FieldSource("TEST_CASES")
+    void byLoopAndCheck(TestCase testCase) {
+        assertEquals(MergeStringsAlternately.byLoopAndCheck(testCase.word1(), testCase.word2()), testCase.expected());
+    }
+
+    @ParameterizedTest
+    @FieldSource("TEST_CASES")
+    void commonAndThenRest(TestCase testCase) {
+        assertEquals(MergeStringsAlternately.commonAndThenRest(testCase.word1(), testCase.word2()), testCase.expected());
+    }
+
+    record TestCase(String word1, String word2, String expected) {
+    }
+}
+
